781ab24d66f793d518e03d66720beac1ab7c78a4
Yoné Toussaint <147109802+yonetoussaint@users.noreply.github.com>
2026-09-09 09:44:55 -0400

    Update DepositPanel.tsx

--- src/WalletPage/DepositPanel.tsx
+++ src/WalletPage/DepositPanel.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { supabase } from "../../lib/supabaseClient";
+import { supabase } from "@/lib/supabaseClient";
 import { DepositHeader } from "./DepositPanel/DepositHeader";
 import { DepositProgress } from "./DepositPanel/DepositProgress";
 import { AmountStep } from "./DepositPanel/AmountStep";
